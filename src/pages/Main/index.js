9bb959b004d9561381ba9baf680419fcc4bc116e
Lipe <feventurelli@hotmail.com>
2022-10-16 19:54:19 -0300

    Estilização global e da página main

---
+++ src/pages/Main/index.js
@@ -0,0 +1,30 @@
+
+import React from "react";
+import { FaGithub, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
+
+export default function Main(){
+    return(
+        <div>
+            <Container>
+                
+                <h1>
+                    <FaGithub size={25}/>
+                    Meus repositórios
+                </h1>
+
+                <Form onSubmit={()=>{}}>
+                    <input type="text" placeholder="Adicionar repositórios" />
+
+                    <SubmitButton>
+                        <FaPlus color="#FFF" size={14} />
+                    </SubmitButton>
+
+                </Form>
+
+            </Container>
+        </div>
+    );
+}
+
