7bee95277600247f18ce16d720f18b2abc015385
Lipe <feventurelli@hotmail.com>
2022-10-20 21:43:04 -0300

    listando e salvando repositorios e carregando detalhes

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import api from '../../services/api';
+
+import { Container } from './styles';
+
 
 export default function Repositorio(){
+
+    const [repo, setRepo] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    let { repositorio } = useParams();
+
+    useEffect(()=>{
+
+        async function load(){
+            const nomeRepo = repositorio;
+
+            const [repositorioData, issuesData] =  await Promise.all([
+                api.get(`/repos/${nomeRepo}`),
+                api.get(`/repos/${nomeRepo}/issues`, {
+                    state: 'open',
+                    per_page: 5
+                })
+            ])
+
+            setRepo(repositorioData.data);
+            setIssues(issuesData.data);
+            setLoading(false);
+        }
+
+        load();
+
+    }, [repositorio]);
+
     return(
-        <div>
-            <h1>Página Repositório</h1>
-        </div>
+        <Container>
+            { repositorio }
+        </Container>
     );
 }
